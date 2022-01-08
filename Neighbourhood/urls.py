8a4cb91e74b9b52a247c5a47d67a10fbbf5e8624
Annaliskirwa <annaliskirwa@gmail.com>
2022-01-08 18:55:30 +0300

    Added timyce to urls and installed apps

--- Neighbourhood/urls.py
+++ Neighbourhood/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('accounts/register/',neighbour_views.register, name='register'),
     path('accounts/login/',auth_views.LoginView.as_view(redirect_authenticated_user=True), name='login'),
     path('logout/',auth_views.LogoutView.as_view(), name='logout'),
+    path('tinymce /', include('tinymce.urls')),
 ]
