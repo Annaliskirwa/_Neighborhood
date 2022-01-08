7f6059a5ea343423566e8a4391df020de6eece24
Annaliskirwa <annaliskirwa@gmail.com>
2022-01-08 19:02:10 +0300

    Created the profile and user profile paths in app

--- Neighbourhood_app/urls.py
+++ Neighbourhood_app/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns=[
     path('',views.index,name='home'),
+    path('profile/<username>/', views.profile, name='profile'),
+    path('user_profile/<username>/', views.user_profile, name='user_profile'),
+
 ]
