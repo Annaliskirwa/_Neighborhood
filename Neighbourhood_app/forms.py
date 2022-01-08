9d70e5fcb710fc6f7022a9b943bbaaa3e698f2b1
Annaliskirwa <annaliskirwa@gmail.com>
2022-01-08 21:02:06 +0300

    User registration and hood registration form

---
+++ Neighbourhood_app/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.forms import fields
+from django.forms.widgets import EmailInput, Textarea
+from . models import NeighbourHood,Post,Business,Profile
+
+# User registration form
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+    
+    class Meta:
+        model = User
+        fields= ['username','email','password1','password2']
+        
+User._meta.get_field('email')._unique=True
+
+# Hood registration form
+class HoodForm(forms.ModelForm):
+    class Meta:
+        model = NeighbourHood
+        exclude = ('admin',)
+        
--- Neighbourhood_app/views.py
+++ Neighbourhood_app/views.py
@@ -1,4 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404, redirect
+from django.contrib.auth.models import User
+from django.http  import HttpResponseRedirect,Http404
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+from django.forms.models import model_to_dict
+from django.contrib import messages
+from .forms import UpdateUserForm, UpdateUserProfileForm, UserRegisterForm,HoodForm,BusinessForm,PostForm
+from .models import NeighbourHood,Business,Post,Profile
+
 
 # Create your views here.
 
