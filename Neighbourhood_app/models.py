00f13d8336e7ba9c2d06ac415a75e97b0de4d728
Annaliskirwa <annaliskirwa@gmail.com>
2022-01-08 16:55:30 +0300

    Created the neighborhood model properies

--- Neighbourhood_app/models.py
+++ Neighbourhood_app/models.py
@@ -5,3 +5,13 @@ from django.db.models.signals import post_save
 from phone_field import PhoneField
 
 # Create your models here.
+# The neighbourhood model
+class NeighbourHood(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    admin = models.ForeignKey(User, on_delete=models.CASCADE, related_name="hood",null=True)
+    hood_logo = models.ImageField(upload_to='images/')
+    description = models.TextField()
+    health_tell = PhoneField(null=True, blank=True)
+    police_number = PhoneField(null=True, blank=True)
+    area_administrator = models.CharField(max_length=100,null=True)
