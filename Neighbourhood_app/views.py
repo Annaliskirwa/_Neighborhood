87dc6e02dd38d4fae0fca8e59ca011e38cfa00be
Annaliskirwa <annaliskirwa@gmail.com>
2022-01-08 21:39:11 +0300

    The user registration view

--- Neighbourhood_app/views.py
+++ Neighbourhood_app/views.py
@@ -11,5 +11,21 @@ from .models import NeighbourHood,Business,Post,Profile
 
 # Create your views here.
 
+# The index view
 def index(request):
     return render(request,'all-neighbour/home.html')
+
+# The user registration view
+def register(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    if request.method=="POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            return redirect('login')
+            
+    else:
+        form = UserRegisterForm()
+    return render(request,"registration/register.html",{'form':form})
