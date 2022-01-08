cb47706932a1751e91efe66058255651dc275453
Annaliskirwa <annaliskirwa@gmail.com>
2022-01-08 21:46:59 +0300

    The profile view

--- Neighbourhood_app/views.py
+++ Neighbourhood_app/views.py
@@ -29,3 +29,29 @@ def register(request):
     else:
         form = UserRegisterForm()
     return render(request,"registration/register.html",{'form':form})
+
+
+# Hoods view
+@login_required(login_url='login')
+def hoods(request):
+    current_user=request.user
+    hoods = NeighbourHood.objects.all()
+    return render(request,"all-neighbour/hoods.html",{'hoods':hoods,'current_user':current_user})
+
+# Profile view
+@login_required(login_url='login')
+def profile(request, username):
+    current_user=request.user
+           
+    if request.method == 'POST':
+        user_form = UpdateUserForm(request.POST, instance=request.user)
+        profile_form = UpdateUserProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            return HttpResponseRedirect(request.path_info)
+    else:
+        user_form = UpdateUserForm(instance=request.user)
+        profile_form = UpdateUserProfileForm(instance=request.user.profile)
+
+    return render(request, 'all-neighbour/profile.html', {'user_form':user_form,'profile_form':profile_form})
