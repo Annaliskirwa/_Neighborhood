63cdfdc7f84150879a8a66bb928b5d8d73cb168c
Annaliskirwa <annaliskirwa@gmail.com>
2022-01-08 21:56:00 +0300

    Add a post view function

--- Neighbourhood_app/views.py
+++ Neighbourhood_app/views.py
@@ -109,3 +109,21 @@ def new_business(request,id):
     else:
         bus_form = BusinessForm()
     return render(request,'all-neighbour/business.html',{'bus_form':bus_form,'title':title})
+
+
+# Add a post view
+@login_required(login_url='login')
+def new_post(request,id):
+    hood = NeighbourHood.objects.get(id=id)
+    title = 'Add here your post'
+    if request.method=='POST':
+        post_form = PostForm(request.POST)
+        if post_form.is_valid():
+            post = post_form.save(commit=False)
+            post.neighbourhood = hood
+            post.owner = request.user
+            post.save()
+            return redirect('my_hood', id)
+    else:
+        post_form = PostForm()
+    return render(request,'all-neighbour/post.html',{'post_form':post_form,'title':title})
