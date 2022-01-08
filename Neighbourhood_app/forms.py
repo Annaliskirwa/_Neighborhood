59e77c05c32afd34e0a65e1bffad5ffcf19d5f46
Annaliskirwa <annaliskirwa@gmail.com>
2022-01-08 21:04:14 +0300

    Update user form

--- Neighbourhood_app/forms.py
+++ Neighbourhood_app/forms.py
@@ -20,4 +20,12 @@ class HoodForm(forms.ModelForm):
     class Meta:
         model = NeighbourHood
         exclude = ('admin',)
-        
+
+
+# Update user form
+class UpdateUserForm(forms.ModelForm):
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email')
