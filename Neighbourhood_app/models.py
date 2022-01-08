a616dfaa469a18c2cab32a905259236e9d598832
Annaliskirwa <annaliskirwa@gmail.com>
2022-01-08 16:52:24 +0300

    modified project requirements, installed phone field and created model imports

--- Neighbourhood_app/models.py
+++ Neighbourhood_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from phone_field import PhoneField
 
 # Create your models here.
