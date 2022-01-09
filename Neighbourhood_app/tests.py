e877ac8e180547f7246e0741e41bfa91cd5e3169
Annaliskirwa <annaliskirwa@gmail.com>
2022-01-09 16:58:57 +0300

    Created test cases for the post model

--- Neighbourhood_app/tests.py
+++ Neighbourhood_app/tests.py
@@ -5,12 +5,12 @@ from django.contrib.auth.models import User
 import datetime as dt
 
 # Create your tests here.
-class neighbourhoodTestClass(TestCase):
+class NeighbourHoodTestClass(TestCase):
     def setUp(self):
         self.Pioneer = NeighbourHood(neighbourhood='Pioneer')
 
     def test_instance(self):
-        self.assertTrue(isinstance(self.Kahawa,NeighbourHood))
+        self.assertTrue(isinstance(self.Pioneer,NeighbourHood))
 
     def tearDown(self):
         NeighbourHood.objects.all().delete()
@@ -24,3 +24,23 @@ class neighbourhoodTestClass(TestCase):
         self.Pioneer.delete_neighbourhood('Pioneer')
         hood = NeighbourHood.objects.all()
         self.assertTrue(len(hood)==0)
+
+class PostTestClass(TestCase):
+    def setUp(self):
+        self.Vaccinate = Post(vaccinate='Vaccinate')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.Vaccinate))
+
+    def tearDown(self):
+        Post.objects.all().delete()
+
+    def test_save_method(self):
+        self.Vaccinate.save_vaccinate()
+        rest = Post.objects.all()
+        self.assertTrue(len(rest)>0)
+
+    def test_delete_method(self):
+        self.Vaccinate.delete_vaccinate('Vaccinate')
+        rest = Post.objects.all()
+        self.assertTrue(len(rest)==0)
